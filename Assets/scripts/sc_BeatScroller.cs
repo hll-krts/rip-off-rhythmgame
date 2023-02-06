5b49dd0a8f716f1543488575eb4d97bb5650f5cc
hll-krts <56218889+hll-krts@users.noreply.github.com>
2023-02-06 15:19:34 +0300

    Add files via upload

---
+++ Assets/scripts/sc_BeatScroller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_BeatScroller : MonoBehaviour
+{
+    public float Tempo;
+    public bool StartControl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tempo = Tempo / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!StartControl)
+        {
+            /*if (Input.anyKeyDown)
+            {
+                StartControl = true;
+            }*/
+        }
+        else
+        {
+            transform.position -= new Vector3(Tempo * Time.deltaTime, 0f, 0f);
+        }
+    }
+}
