3ba4498c94df504083d29caa91ce4b5f867794a7
hll-krts <khalil.karatas@gmail.com>
2023-02-17 18:21:37 +0300

    failed to create a txt file miserably
    but hey, it gets notes' coordinates

--- Assets/scripts/SongScripts/sc_NotePosSave.cs
+++ Assets/scripts/SongScripts/sc_NotePosSave.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 public class sc_NotePosSave : MonoBehaviour
 {
     // Start is called before the first frame update
-    GameObject[] UpNotes, DownNotes, MirrorNotes;
-    string[] json;
+    public GameObject[] UpNotes, DownNotes, MirrorNotes;
+    string[] mirrorNote, upNote, downNote;
     void Start()
     {
         /*NotePos notePosition = new NotePos{
@@ -17,15 +19,11 @@ public class sc_NotePosSave : MonoBehaviour
         UpNotes = GameObject.FindGameObjectsWithTag("noteUp");
         DownNotes = GameObject.FindGameObjectsWithTag("noteDown");
         MirrorNotes = GameObject.FindGameObjectsWithTag("mirrorNote");
-        for (int i = 0; i < MirrorNotes.Length; i++)
-        {
-            NotePos notePosition = new NotePos
-            {
-                notePos = MirrorNotes[i].transform.position
-            };
-            json[i] = JsonUtility.ToJson(notePosition);
-        }
-        Debug.Log(json);
+        SaveUpNotePos();
+        SaveDownNotePos();
+        SaveMirrorNotePos();
+
+        SaveToFile();
     }
 
     // Update is called once per frame
@@ -33,14 +31,61 @@ public class sc_NotePosSave : MonoBehaviour
     {
 
     }
-
-    private void SaveNotePos()
+    private void SaveToFile()
     {
+        using (StreamWriter sw = File.CreateText(Application.dataPath))
+        {
 
+        };
+    }
+    private void SaveUpNotePos()
+    {
+        upNote = new string[UpNotes.Length];
+        for (int i = 0; i < UpNotes.Length; i++)
+        {
+            NotePos upNotePosition = new NotePos
+            {
+                Name = UpNotes[i].name,
+                notePos = UpNotes[i].transform.position
+            };
+            upNote[i] = JsonUtility.ToJson(upNotePosition);
+        }
+    }
+    private void SaveDownNotePos()
+    {
+        downNote = new string[DownNotes.Length];
+        for (int i = 0; i < DownNotes.Length; i++)
+        {
+            NotePos downNotePosition = new NotePos
+            {
+                Name = DownNotes[i].name,
+                notePos = DownNotes[i].transform.position
+            };
+            downNote[i] = JsonUtility.ToJson(downNotePosition);
+        }
+    }
+
+    private void SaveMirrorNotePos()
+    {
+        mirrorNote = new string[MirrorNotes.Length];
+        for (int i = 0; i < MirrorNotes.Length; i++)
+        {
+            NotePos mirrorNotePosition = new NotePos
+            {
+                Name = MirrorNotes[i].name,
+                notePos = MirrorNotes[i].transform.position
+            };
+            mirrorNote[i] = JsonUtility.ToJson(mirrorNotePosition);
+        }
+        /*foreach (string value in mirrorNote)
+        {
+            Debug.Log(value);
+        }*/
     }
 
     public class NotePos
     {
-        public Vector2 notePos;
+        public string Name;
+        public Vector3 notePos;
     }
 }
